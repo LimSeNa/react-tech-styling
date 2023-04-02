ed2013076253250f59346309f39d79b242be4da3
Sena Lim <tpsk8322@naver.com>
2023-04-03 01:24:18 +0900

    additionalData 옵션 추가

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -513,19 +513,23 @@ module.exports = function (webpackEnv) {
                   sourceMap: isEnvProduction
                     ? shouldUseSourceMap
                     : isEnvDevelopment,
+                  modules: {
+                    mode: 'icss',
+                  }
                 }).concat({
                   loader: require.resolve('sass-loader'),
                   options: {
                     sassOptions: {
                       includePaths: [paths.appSrc + '/styles'],
                     },
+                    additionalData: `@import 'utils';`
                   }
                 }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
               // See https://github.com/webpack/webpack/issues/6571
-              sideEffects: true,
+              sideEffects: true
             },
             // Adds support for CSS Modules, but using SASS
             // using the extension .module.scss or .module.sass
